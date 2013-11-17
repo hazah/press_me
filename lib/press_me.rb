f4a572b016d46762d5ff60861a54525c2ab0ffa1
Ivgeni Slabkovski <zhenya@zhenya.ca>
2013-11-17 00:22:30 -0500

    Attempts to resolve test failures.

--- lib/press_me.rb
+++ lib/press_me.rb
@@ -12,9 +12,8 @@ module PressMe
 
           super.tap do |definition|
             singleton_class.send(:define_method, name) do |*args|
-              definition.call(*args).tap do |scope|
-                auth.scope = scope.extending(extension)
-              end
+              scope = definition.call(*args)
+              auth.scope = scope.extending(extension)
             end
           end
         end
